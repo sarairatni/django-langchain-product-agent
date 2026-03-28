2cb7be9906c8ea900d823de6c0b7eb5bb13bf945
sarairatni <ms_iratni@esi.dz>
2026-03-28 02:34:36 +0100

    adding prject

--- sri_project/urls.py
+++ sri_project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('agent_service.urls')),
-]
+    path('api/agent', include('agent_service.urls')),
+    path('', include('agent_service.urls')),]
